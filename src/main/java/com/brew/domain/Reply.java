59938b2451d268f6986d00ca95b9bd10718b67cf
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2022-04-21 09:50:46 +0900

    main이랑 merge 완료

--- src/main/java/com/brew/domain/Reply.java
+++ src/main/java/com/brew/domain/Reply.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.brew.domain;
 
 import javax.persistence.Column;
@@ -59,65 +58,3 @@ public class Reply {
 	
 
 }
-=======
-package com.brew.domain;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@Entity
-@Table
-public class Reply {
-	
-	@ManyToOne
-	@JoinColumn(name="board_id")
-	private Board board;
-	
-	@Id
-	@NotNull
-	@Column
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String replyId;
-	
-	@NotNull
-	@Column
-	private String replyContent;
-	
-	@NotNull
-	@Column
-	private String replyUser;
-	
-	@Column
-	private String replyDate;
-	
-	@Column
-	private Integer replyLikeCount;
-
-	@Builder
-	public Reply(@NotNull String replyId, @NotNull String replyContent,
-			@NotNull String replyUser, String replyDate, Integer replyLikeCount) {
-		super();
-		this.replyId = replyId;
-		this.replyContent = replyContent;
-		this.replyUser = replyUser;
-		this.replyDate = replyDate;
-		this.replyLikeCount = replyLikeCount;
-	}
-	
-	
-
-}
->>>>>>> refs/remotes/origin/main
--- src/main/java/com/brew/repository/BoardRepository.java
+++ src/main/java/com/brew/repository/BoardRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.brew.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,16 +9,3 @@ import com.brew.domain.Board;
 public interface BoardRepository extends JpaRepository<Board, String>{
 
 }
-=======
-package com.brew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.brew.domain.Board;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, String>{
-
-}
->>>>>>> refs/remotes/origin/main
