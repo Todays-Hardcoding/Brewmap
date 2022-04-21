3761dfb2c28a5d8acc639628978877cf3a0cc953
Jinho Kim <wlsghrla94@gmail.com>
2022-04-21 10:20:30 +0900

    메인copy

--- src/main/java/com/brew/domain/LatLon.java
+++ src/main/java/com/brew/domain/LatLon.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.brew.domain;
 
 import lombok.Data;
@@ -8,14 +7,3 @@ public class LatLon {
 	private String lat;
 	private String lon;
 }
-=======
-package com.brew.domain;
-
-import lombok.Data;
-
-@Data
-public class LatLon {
-	private String lat;
-	private String lon;
-}
->>>>>>> branch 'jinho_branch' of https://github.com/Todays-Hardcoding/Brewmap.git
--- src/main/java/com/brew/repository/BoardRepository.java
+++ src/main/java/com/brew/repository/BoardRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.brew.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,16 +9,3 @@ import com.brew.domain.Board;
 public interface BoardRepository extends JpaRepository<Board, String>{
 
 }
-=======
-package com.brew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.brew.domain.Board;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, String>{
-
-}
->>>>>>> branch 'jinho_branch' of https://github.com/Todays-Hardcoding/Brewmap.git
