e939b94ff2f75d298fbc9555ad0f3ce4b0521541
kyunghee <pkh98@DESKTOP-LTRGI84>
2022-04-22 15:15:54 +0900

    악

--- src/main/java/com/brew/domain/User.java
+++ src/main/java/com/brew/domain/User.java
@@ -1,27 +1,21 @@
 package com.brew.domain;
 
 import java.time.LocalDateTime;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import java.util.List;
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-<<<<<<< HEAD
-=======
+
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
+
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-<<<<<<< HEAD
-=======
+
 import groovy.transform.ToString;
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -116,116 +110,3 @@ public class User {
 	
 	
 
-	
-
-	
-=======
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.DynamicUpdate;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@Entity
-@Table
-@DynamicUpdate
-public class User {
-	@OneToMany(mappedBy = "user")
-	private List<Board> board;
-
-	@NotNull
-	@Column
-	private String userCategory;
-	@Id
-	@NotNull
-	@Column
-	private String userId;
-	@NotNull
-	@Column
-	private String userPw;
-	@NotNull
-	@Column
-	private String userName;
-	@NotNull
-	@Column
-	private String userTel;
-	@NotNull
-	@Column
-	private String userEmail;
-	@NotNull
-	@Column
-	private String userGrade;
-	@Column
-	private LocalDateTime userRegDate;
-	@PrePersist
-<<<<<<< HEAD
-	public void createdAt() {
-=======
-	public void createDate() {
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
-		this.userRegDate = LocalDateTime.now();
-	}
-	@NotNull
-	@Column
-	private String userBirthDate;
-	@NotNull
-	@Column
-	private String userNickName;
-	@NotNull
-	@Column
-	private boolean userGender;
-	@Column
-	private String userCoupon;
-	@Column
-	private String userPoint;
-	@Column
-	private String userQuestion;
-	@Column
-	private String userAnswer;
-	
-	@Builder
-	public User(List<Board> board, @NotNull String userCategory, @NotNull String userId, @NotNull String userPw,
-			@NotNull String userName, @NotNull String userTel, @NotNull String userEmail, @NotNull String userGrade,
-			LocalDateTime userRegDate, @NotNull String userBirthDate, @NotNull String userNickName,
-<<<<<<< HEAD
-			@NotNull boolean userGender, String userCoupon, int userPoint, String userQuestion, String userAnswer) {
-=======
-			@NotNull boolean userGender, String userCoupon, String userPoint, String userQuestion, String userAnswer) {
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
-		super();
-		this.board = board;
-		this.userCategory = userCategory;
-		this.userId = userId;
-		this.userPw = userPw;
-		this.userName = userName;
-		this.userTel = userTel;
-		this.userEmail = userEmail;
-		this.userGrade = userGrade;
-		this.userRegDate = userRegDate;
-		this.userBirthDate = userBirthDate;
-		this.userNickName = userNickName;
-		this.userGender = userGender;
-		this.userCoupon = userCoupon;
-		this.userPoint = userPoint;
-		this.userQuestion = userQuestion;
-		this.userAnswer = userAnswer;
-	}
-<<<<<<< HEAD
->>>>>>> branch 'main' of https://github.com/Todays-Hardcoding/Brewmap.git
-=======
-	
->>>>>>> branch 'GY' of https://github.com/Todays-Hardcoding/Brewmap.git
-
-}
