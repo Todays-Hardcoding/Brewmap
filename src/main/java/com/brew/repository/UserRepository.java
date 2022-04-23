f2f8e03b05347b9d90f9eec9fa2ead184a18dbf2
Almondshin <Almondshin8@gmail.com>
2022-04-23 17:23:10 +0900

    로그인 및 회원가입 임시
    회원가입 -> DB에 등록됨
    로그인 -> id만 입력해도 로그인됨
    (비밀번호도 db정보랑 매칭되면 로그인되게 바꿔야함)

--- src/main/java/com/brew/repository/UserRepository.java
+++ src/main/java/com/brew/repository/UserRepository.java
@@ -9,6 +9,22 @@ import com.brew.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-	public User findByUserId(String userId);
 	
+	
+	public User findByUserId(String userId);
+
+	public User findByUserName(String userName);
+
+	public User findByUserBirthDate(String userBirthDate);
+
+	public User findByUserGrade(String userGrade);
+
+	public User findByUserTel(String userTel);
+
+	public User findByUserEmail(String userEmail);
+	
+	
+//	@Query(value="Update user set userId = :userId where userId = :userId ")
+//	public User changeByUserId(@Param("userId") String userId);
+
 }
--- src/main/java/com/brew/service/UserService.java
+++ src/main/java/com/brew/service/UserService.java
@@ -1,6 +1,7 @@
 package com.brew.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -16,10 +17,12 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	// 유저 전체조회
 	public List<User> findAllUser() {
 		return userRepository.findAll();
 	}
 
+	// 유저 id 조회
 	public User findByUserId(String userId) {
 		return userRepository.findByUserId(userId);
 	}
@@ -35,6 +38,31 @@ public class UserService {
 	
 	public void delete(User user) {
 		userRepository.delete(user);
+	}
+	
+	public void insert(User user) {
+		userRepository.save(user);
+		
+	};
+	
+	@Transactional
+	public String join(User user) {
+			try {
+				validateDuplicateMember(user);
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			userRepository.save(user);
+			return user.getUserId();
+	}
+	
+	
+	//중복체크 id
+	private void validateDuplicateMember(User user) throws IllegalAccessException {
+		Optional<User> findUser = userRepository.findById(user.getUserId());
+		if(!findAllUser().isEmpty()) {
+			throw new IllegalAccessException("이미 존재하는 회원입니다.");
+		}
 	}
 	
 }
