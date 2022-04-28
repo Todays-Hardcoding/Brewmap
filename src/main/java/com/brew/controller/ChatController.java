18107a53cf2c90dc70619de27830d2d221b3935d
HazZlng <63578131+HazZlng@users.noreply.github.com>
2022-04-28 21:46:47 +0900

    채팅방 완.벽.구.현

--- src/main/java/com/brew/controller/ChatController.java
+++ src/main/java/com/brew/controller/ChatController.java
@@ -1,58 +1,88 @@
 package com.brew.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.brew.domain.ChatRoom;
-import com.brew.domain.ChatRoomForm;
-import com.brew.domain.User;
-import com.brew.repository.ChatRoomRepository;
+import com.brew.domain.Room;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
-    private final ChatRoomRepository chatRoomRepository;
-
-    @GetMapping("/chatting")
-    public String rooms(Model model){
-        model.addAttribute("rooms",chatRoomRepository.findAllRoom());
-        System.out.println(chatRoomRepository.findAllRoom());
-        return "view/chat/rooms";
-    }
-
-    @GetMapping("/rooms/{id}")
-    public String room(@PathVariable String id, Model model, HttpServletRequest req){
-        ChatRoom room = chatRoomRepository.findRoomById(id);
-        User user = (User)req.getSession().getAttribute("user");
-        
-        model.addAttribute("room",room);
-        if(user.getUserNickName() != null)
-        	model.addAttribute("userNickName", user.getUserNickName());
-        else
-        	model.addAttribute("userNickName", "nullUserNickname");
-//        System.out.println(user);
-        return "view/chat/room";
-    }
-
-    @GetMapping("/new")
-    public String make(Model model){
-        ChatRoomForm form = new ChatRoomForm();
-        model.addAttribute("form",form);
-        return "view/chat/newRoom";
-    }
-
-    @PostMapping("/room/new")
-    public String makeRoom(ChatRoomForm form){
-        chatRoomRepository.createChatRoom(form.getName());
-
-        return "redirect:/chatting";
-    }
-
+	List<Room> roomList = new ArrayList<Room>();
+	static int roomNumber = 0;
+	
+	@RequestMapping("/chat")
+	public ModelAndView chat() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("view/chat/chat");
+		return mv;
+	}
+	
+	/**
+	 * 방 페이지
+	 * @return
+	 */
+	@RequestMapping("/room")
+	public ModelAndView room() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("view/chat/room");
+		return mv;
+	}
+	
+	/**
+	 * 방 생성하기
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/createRoom")
+	public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){
+		String roomName = (String) params.get("roomName");
+		if(roomName != null && !roomName.trim().equals("")) {
+			Room room = new Room();
+			room.setRoomNumber(++roomNumber);
+			room.setRoomName(roomName);
+			roomList.add(room);
+		}
+		return roomList;
+	}
+	
+	/**
+	 * 방 정보가져오기
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/getRoom")
+	public @ResponseBody List<Room> getRoom(@RequestParam HashMap<Object, Object> params){
+		return roomList;
+	}
+	
+	/**
+	 * 채팅방
+	 * @return
+	 */
+	@RequestMapping("/moveChating")
+	public ModelAndView chating(@RequestParam HashMap<Object, Object> params) {
+		ModelAndView mv = new ModelAndView();
+		int roomNumber = Integer.parseInt((String) params.get("roomNumber"));
+		
+		List<Room> new_list = roomList.stream().filter(o->o.getRoomNumber()==roomNumber).collect(Collectors.toList());
+		if(new_list != null && new_list.size() > 0) {
+			mv.addObject("roomName", params.get("roomName"));
+			mv.addObject("roomNumber", params.get("roomNumber"));
+			mv.setViewName("view/chat/chat");
+		}else {
+			mv.setViewName("view/chat/room");
+		}
+		return mv;
+	}
 }
