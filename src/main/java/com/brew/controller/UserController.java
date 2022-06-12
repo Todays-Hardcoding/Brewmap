ad109d03f23f3e2b14d073bbe0d71f94c26d94f4
Almondshin <Almondshin8@gmail.com>
2022-06-12 23:50:55 +0900

    예외처리 예시

--- src/main/java/com/brew/controller/UserController.java
+++ src/main/java/com/brew/controller/UserController.java
@@ -1,17 +1,30 @@
 package com.brew.controller;
 
+import java.nio.charset.Charset;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.brew.domain.User;
 import com.brew.service.BoardService;
 import com.brew.service.UserService;
+import com.zidol.fc.controller.DataResponse;
+import com.zidol.fc.controller.StatusCode;
+import com.zidol.fc.error.ErrorResponse;
+import com.zidol.fc.error.user.DuplicateIdException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -56,4 +69,33 @@ public class UserController {
 		session.removeAttribute("user");
 		return "view/index";
 	}
+
+// 예외처리 예시
+//	@PostMapping("/sign-up.act")
+//	public ResponseEntity<DataResponse> signUp(@RequestBody Map<String, String> params) {		
+//		DataResponse dataResponse = new DataResponse();
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+//
+//		User user = User.builder().userEmail(params.get("userEmail")).userPassword(params.get("userPassword"))
+//				.userName(params.get("userName")).userNickname(params.get("userNickname")).build();
+//		
+//		if(userService.findByUserEmail(params.get("userEmail")) != null) {
+//			ErrorResponse.CustomFieldError customFieldError = new ErrorResponse.CustomFieldError("User", params.get("userEmail"), "아이디 중복");
+//			throw new DuplicateIdException(customFieldError);
+//		}
+//		
+//		if (userService.insertUser(user) != null) {
+//			dataResponse.setStatus(StatusCode.OK.getStatus());
+//			dataResponse.setCode(StatusCode.OK.getCode());
+//			dataResponse.setData(user);
+//			
+//			return new ResponseEntity<DataResponse>(dataResponse, headers, HttpStatus.OK);
+//		} else {
+//			dataResponse.setStatus(StatusCode.NOT_FOUND.getStatus());
+//			dataResponse.setCode(StatusCode.NOT_FOUND.getCode());
+//			
+//			return new ResponseEntity<DataResponse>(dataResponse, headers, HttpStatus.NOT_FOUND);
+//		}
+//	}
 }
